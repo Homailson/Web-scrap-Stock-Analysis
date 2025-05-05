52c64c58b2a2e47cac9801a5fa90aa076df5412e
Homailson <homailson@gmail.com>
2025-05-05 16:19:46 -0300

    ajustando detalhes de porta

--- app.py
+++ app.py
@@ -130,4 +130,4 @@ def atualizar_noticias(empresa):
 server = app.server
 
 port = int(os.environ.get("PORT", 8050))  # Render define PORT
-    app.run(host='0.0.0.0', port=port, debug=False)
+app.run(host='0.0.0.0', port=port, debug=False)
